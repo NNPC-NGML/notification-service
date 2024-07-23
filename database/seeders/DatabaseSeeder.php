6331efe476cc8ec9ede84d75f78a893b4a02b03c
Colin-nwk <collinsnwoko05@gmail.com>
2024-07-23 22:47:48 +0100

    fixed seeder error

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,10 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        DB::table('notification_tasks')->truncate();
+
+        Event::fakeFor(function () {
+            // Create models without triggering events
+            // YourModel::factory()->count(5)->create();
+            \App\Models\NotificationTask::factory(50)->create();
+        });
+
+
         // \App\Models\User::factory(10)->create();
         // \App\Models\User::factory(10)->create();
 
-        \App\Models\NotificationTask::factory(50)->create();
+        // \App\Models\NotificationTask::factory(50)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
